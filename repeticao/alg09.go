656dca55ba7049875bbf299ec0caadbb566705f1
karlosmafra <kadumaframoraes@gmail.com>
2023-03-21 21:46:06 -0300

    alg09

--- repeticao/alg09.go
+++ repeticao/alg09.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var nums = []int{}
+	fmt.Print("Digite números inteiros: ")
+	//ler valores
+	fmt.Scan(&nums)
+	fmt.Println(nums)
+	soma := 0
+	for i := 0; i < len(nums); i++ {
+		soma += nums[i]
+		if nums[i] == 0 {
+			break
+		}
+	}
+	med := soma / len(nums)
+	fmt.Println("A média é igual a", med)
 }
